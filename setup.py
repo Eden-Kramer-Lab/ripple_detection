84f0f0466ae26997fa1dbf9b3b09c3ecf9f6ec37
Eric Denovellis <edeno@bu.edu>
2024-10-16 16:29:30 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ TESTS_REQUIRE = ["pytest >= 2.7.1"]
 
 setup(
     name="ripple_detection",
-    version="1.5.0",
+    version="1.5.1",
     license="MIT",
     description=(
         "Tools for finding sharp-wave ripple events (150-250 Hz) "
